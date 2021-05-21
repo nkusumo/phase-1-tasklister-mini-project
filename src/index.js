64da49fde5a2fa50f2aecae762e468c7f7de5ece
nkusumo <nis.kusumo@gmail.com>
2021-05-21 12:39:49 -0700

    added event listener with console log

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('submitted');
+  })
 });
+
+// function addTask(event) {
+//   let taskDescription = event.target.new-task-description.value
+//   let newTask = document.createElement('li');
+//   newTask.textContent = taskDescription;
+//   document.getElementById('tasks').append(newTask)
+// }
